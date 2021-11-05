dc0eaff6beecc0edf97a27bc54b69c292aa92af6
shentuzhigang <shentuzhigang@moresec.cn>
2021-11-05 17:42:11 +0800

    chore(vuepress-plugin-github-corner): move some css to plugin

--- packages/@starzkg/vuepress-plugin-github-corner/src/client/clientAppEnhance.ts
+++ packages/@starzkg/vuepress-plugin-github-corner/src/client/clientAppEnhance.ts
@@ -1,6 +1,6 @@
 import { defineClientAppEnhance } from '@vuepress/client'
 import GithubCorner from './components/GithubCorner.vue'
-import './styles/github-corner.css'
+import './styles/github-corner.scss'
 
 export default defineClientAppEnhance(({ app }) => {
   // wrap the `<GithubCorner />` component with plugin options
