7bb085dbbc85cd2c6574ec4b7d0cce7783c25e7b
shentuzhigang <shentuzhigang01@weidian.com>
2022-10-09 21:12:32 +0800

    fix: fix

--- example/.vuepress/locales/en/index.ts
+++ example/.vuepress/locales/en/index.ts
@@ -2,10 +2,10 @@ import navbar from './navbar'
 import sidebar from './sidebar'
 export default {
   // navbar
-  navbar: navbar,
+  navbar,
 
   // sidebar
-  sidebar: sidebar,
+  sidebar,
 
   // page meta
   editLinkText: 'Edit this page on GitHub',
