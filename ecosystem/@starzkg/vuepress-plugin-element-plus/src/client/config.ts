e9f7b762a5899e0e2a75de8c30d493a331e8b88e
shentuzhigang <shentuzhigang01@weidian.com>
2022-10-13 17:19:12 +0800

    feat: element-plus dark mode

--- ecosystem/@starzkg/vuepress-plugin-element-plus/src/client/config.ts
+++ ecosystem/@starzkg/vuepress-plugin-element-plus/src/client/config.ts
@@ -2,6 +2,7 @@ import { default as ElementPlusIcons } from '@element-plus/icons-vue/global'
 import { defineClientConfig } from '@vuepress/client'
 import ElementPlus from 'element-plus'
 import 'element-plus/theme-chalk/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
 
 export default defineClientConfig({
   enhance: ({ app }) => {
