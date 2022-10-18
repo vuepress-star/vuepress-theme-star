4ee0875633c5220347c831cf66ee7f85c2be236d
shentuzhigang <shentuzhigang01@weidian.com>
2022-10-18 22:13:38 +0800

    fix: fix

--- packages/@starzkg/vuepress-star-shared/src/node/index.ts
+++ packages/@starzkg/vuepress-star-shared/src/node/index.ts
@@ -1,2 +1,3 @@
 export * from './bundler/index.js'
 export * from './logger.js'
+export * from '../shared/index.js'
--- packages/@starzkg/vuepress-theme-star/src/client/components/PageReadingTime.vue
+++ packages/@starzkg/vuepress-theme-star/src/client/components/PageReadingTime.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { dayjs } from '@starzkg/vuepress-star-shared/client'
 import { usePageData } from '@vuepress/client'
 import { computed } from 'vue'
 import { Time } from '../icons'
@@ -10,9 +11,12 @@ interface ReadingTimePageData {
 const page = usePageData<ReadingTimePageData>()
 
 const readingTime = computed(() => {
-  const readingTime = page.value.readingTime
+  const readingTime = dayjs
+    .duration(page.value.readingTime, 'minutes')
+    .locale(page.value.lang)
+    .humanize()
   if (!readingTime) {
-    return 0
+    return undefined
   }
   return readingTime
 })
