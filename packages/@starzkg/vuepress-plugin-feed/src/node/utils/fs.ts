e68d4e5db57591c9f8004f8770ea5b03b3d0611c
shentuzhigang <shentuzhigang01@weidian.com>
2022-10-22 17:45:01 +0800

    fix: fix

--- packages/@starzkg/vuepress-plugin-feed/src/node/utils/fs.ts
+++ packages/@starzkg/vuepress-plugin-feed/src/node/utils/fs.ts
@@ -1,11 +1,14 @@
-import { dirname } from 'path'
-import { chalk, fs, logger } from '@vuepress/utils'
+import { chalk, fs, path, withSpinner } from '@vuepress/utils'
 
 export const outputFile = async (
   pathname: string,
   content: string
-): Promise<void> => {
-  await fs.ensureDir(dirname(pathname))
-  await fs.outputFile(pathname, content)
-  logger.info(`file generated and saved to ${chalk.cyan(pathname)}`)
-}
+): Promise<void> =>
+  withSpinner(
+    `Generating feed ${chalk.cyan(path.basename(pathname))} on ${chalk.cyan(
+      pathname
+    )}`
+  )(async () => {
+    await fs.ensureDir(path.dirname(pathname))
+    await fs.outputFile(pathname, content)
+  })
