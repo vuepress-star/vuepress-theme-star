d23a53b5cff00cf84dfcb9c6d2f970563c3d8b4e
shentuzhigang <shentuzhigang01@weidian.com>
2022-10-24 01:24:05 +0800

    fix: fix

--- ecosystem/@starzkg/vuepress-plugin-element-plus/src/client/config.ts
+++ ecosystem/@starzkg/vuepress-plugin-element-plus/src/client/config.ts
@@ -1,4 +1,4 @@
-import { default as ElementPlusIcons } from '@element-plus/icons-vue/global'
+import ElementPlusIcons from '@element-plus/icons-vue/global'
 import { defineClientConfig } from '@vuepress/client'
 import ElementPlus from 'element-plus'
 import 'element-plus/theme-chalk/index.css'
