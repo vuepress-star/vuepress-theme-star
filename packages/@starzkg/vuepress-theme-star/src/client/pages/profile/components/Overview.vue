fa40321790e919f92b67d48d66d584b58f6ae7ea
shentuzhigang <shentuzhigang01@weidian.com>
2022-10-26 16:05:39 +0800

    fix: fix

---
+++ packages/@starzkg/vuepress-theme-star/src/client/pages/profile/components/Overview.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+/* eslint-disable import/first, import/no-duplicates, import/order */
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Overview',
+})
+</script>
+
+<script lang="ts" setup>
+import { Content } from '@theme/components'
+</script>
+
+<template>
+  <div class="overview">
+    <Content />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
--- packages/@starzkg/vuepress-theme-star/src/client/pages/profile/components/Profile.vue
+++ packages/@starzkg/vuepress-theme-star/src/client/pages/profile/components/Profile.vue
@@ -3,13 +3,58 @@ import { TabPane, Tabs } from '@starzkg/vuepress-star-shared/client'
 import {
   Archive,
   CalendarGraph,
-  Content,
   Features,
   Footer,
   Navbar,
   ProfileCard,
   Timeline,
 } from '@theme/components'
+import { defineAsyncComponent, FunctionalComponent, h } from 'vue'
+
+const OrderedTabs: FunctionalComponent = () => {
+  return h(Tabs, {}, () =>
+    [
+      {
+        name: 'Overview',
+        component: defineAsyncComponent({
+          // 加载函数
+          loader: () => import('./Overview.vue'),
+
+          // 加载异步组件时使用的组件
+          // loadingComponent: LoadingComponent,
+          // 展示加载组件前的延迟时间，默认为 200ms
+          delay: 200,
+
+          // 加载失败后展示的组件
+          // errorComponent: ErrorComponent,
+          // 如果提供了一个 timeout 时间限制，并超时了
+          // 也会显示这里配置的报错组件，默认值是：Infinity
+          timeout: 3000,
+        }),
+      },
+      {
+        name: 'Article',
+        component: h('div', { class: 'article-list' }),
+      },
+      {
+        name: 'Projects',
+        component: Features,
+      },
+      {
+        name: 'Calendar',
+        component: CalendarGraph,
+      },
+      {
+        name: 'Timeline',
+        component: Timeline,
+      },
+      {
+        name: 'Archive',
+        component: Archive,
+      },
+    ].map((item) => h(TabPane, { title: item.name }, () => h(item.component)))
+  )
+}
 </script>
 
 <template>
@@ -33,28 +78,8 @@ import {
         <ProfileCard class="mobile-show" />
         <div class="profile-info-list"></div>
       </aside>
-
       <main class="profile-main">
-        <Tabs>
-          <TabPane title="Overview">
-            <Content class="custom" />
-          </TabPane>
-          <TabPane title="Article">
-            <div class="article-list"></div>
-          </TabPane>
-          <TabPane title="Projects">
-            <Features />
-          </TabPane>
-          <TabPane title="Calendar">
-            <CalendarGraph />
-          </TabPane>
-          <TabPane title="Timeline">
-            <Timeline />
-          </TabPane>
-          <TabPane title="Archive">
-            <Archive />
-          </TabPane>
-        </Tabs>
+        <OrderedTabs />
       </main>
     </section>
 
