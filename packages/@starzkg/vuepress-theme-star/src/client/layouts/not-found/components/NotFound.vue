7f82f03b16e20f835d4cfe633302c232f7211d36
shentuzhigang <shentuzhigang01@weidian.com>
2022-11-01 23:04:07 +0800

    fix: fix

--- packages/@starzkg/vuepress-theme-star/src/client/layouts/not-found/components/NotFound.vue
+++ packages/@starzkg/vuepress-theme-star/src/client/layouts/not-found/components/NotFound.vue
@@ -13,9 +13,9 @@ const homeText = themeLocale.value.backToHome ?? 'Back to home'
 </script>
 
 <template>
-  <div class="theme-container">
+  <div class="theme-container theme-star">
     <main class="page">
-      <div class="theme-default-content">
+      <div class="theme-default-content theme-star-content">
         <h1>404</h1>
 
         <blockquote>{{ getMsg() }}</blockquote>
